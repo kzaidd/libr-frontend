d0d95cc6a78c978eccb5c325127c3d20207252d3
Zaid Khan <53516135+kzaidd@users.noreply.github.com>
2023-01-10 18:45:28 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 var url = process.env.BASE_URL;
 if (process.env.NODE_ENV === "production") {
-    url = process.env.BASE_URL + "/api"
+    url = "https://library-backend-api.onrender.com/api"
   console.log(url)
 } else {
     url = "http://localhost:50000/api"
